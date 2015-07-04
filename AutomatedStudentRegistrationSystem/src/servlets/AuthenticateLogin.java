76b681ee833864b584ffec6895cc3958944bf8f2
Aakansha <monstershome@gmail.com>
2015-07-04 14:14:18 +0530

    Added javadoc comments and refactored some code

--- AutomatedStudentRegistrationSystem/src/servlets/AuthenticateLogin.java
+++ AutomatedStudentRegistrationSystem/src/servlets/AuthenticateLogin.java
@@ -1,4 +1,5 @@
 package servlets;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -15,87 +16,93 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class AuthenticeLogin
+ * This servlet checks whether the username,password and designation matches in
+ * the database when it recieves a post request.If matched then it creates a
+ * session and forwards to Welcome servlet otherwise redirects to
+ * ErrorPage.html.On recieving get request it redirects to LogIn servlet.
+ * 
+ * 
+ * @author Aakansha Doshi
+ * 
  */
 @WebServlet("/AuthenticateLogin")
 public class AuthenticateLogin extends HttpServlet {
 	Connection con;
 	Statement stmt;
-	String sql;  
-	
+	String sql;
+
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AuthenticateLogin() {
-        super();
-        // TODO Auto-generated constructor stub
-       con=null;
-       stmt=null;
-       sql=null;
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.sendRedirect("/AutomatedStudentRegistrationSystem/LogIn");
-		//System.out.println("redirected");
-		
+	public AuthenticateLogin() {
+		super();
+		// TODO Auto-generated constructor stub
+		con = null;
+		stmt = null;
+		sql = null;
 	}
 
-	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.sendRedirect("/AutomatedStudentRegistrationSystem/LogIn");
+		// System.out.println("redirected");
+
+	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-		
+
 		try {
 			System.out.println("posty");
 			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("Creating connection");
-			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/d2","root", "Password");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/d2",
+					"root", "Password");
 			System.out.println("Creating Statemnet");
-			stmt=con.createStatement();
-			String user=request.getParameter("Username");
-			String Password=request.getParameter("Password");	
-			String Designation=request.getParameter("Post");
-			System.out.println(user+" "+Password+" "+Designation);
-			String sql="select * from t1 where Username='"+user+"' and Password='"+Password+"' and Designation='"+Designation+"'";
-			ResultSet rs=stmt.executeQuery(sql);			
-			if(rs.next())
-			{
-				
-				HttpSession session=request.getSession(true);
+			stmt = con.createStatement();
+			String user = request.getParameter("Username");
+			String Password = request.getParameter("Password");
+			String Designation = request.getParameter("Post");
+			System.out.println(user + " " + Password + " " + Designation);
+			String sql = "select * from t1 where Username='" + user
+					+ "' and Password='" + Password + "' and Designation='"
+					+ Designation + "'";
+			ResultSet rs = stmt.executeQuery(sql);
+			if (rs.next()) {
+
+				HttpSession session = request.getSession(true);
 				System.out.println("match");
-		 		//session.setAttribute("LOGGEDIN", "true");
-		 		session.setAttribute("USERNAME", user);
-		 		session.setAttribute("DESG",Designation);
-		 		response.sendRedirect("/AutomatedStudentRegistrationSystem/Welcome");
-		 		
-				 
+				// session.setAttribute("LOGGEDIN", "true");
+				session.setAttribute("USERNAME", user);
+				session.setAttribute("DESG", Designation);
+				response.sendRedirect("/AutomatedStudentRegistrationSystem/Welcome");
+
+			} else {
+				RequestDispatcher rd = request
+						.getRequestDispatcher("/WEB-INF/ErrorPage.html");
+				rd.forward(request, response);
 			}
-			else
-			{
-				RequestDispatcher rd=request.getRequestDispatcher("/WEB-INF/ErrorPage.html");
-				rd.forward(request,response);
-			}
-		
-			
-		}
-		catch (ClassNotFoundException e) {
+
+		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
--- AutomatedStudentRegistrationSystem/src/servlets/Welcome.java
+++ AutomatedStudentRegistrationSystem/src/servlets/Welcome.java
@@ -1,5 +1,5 @@
-
 package servlets;
+
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -11,47 +11,52 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class Welcome
+ * This servlet checks the value of session attribute and forwards to desired
+ * page if value is not null otherwise redirects to LogIn servlet on recieving
+ * get request.
+ * 
+ * @author Aakansha Doshi
+ * 
  */
 @WebServlet("/Welcome")
 public class Welcome extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Welcome() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Welcome() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		HttpSession session=request.getSession(false);		
-		if(session==null)
-		{
-			response.sendRedirect("/AutomatedStudentRegistrationSystem/LoginPage.html");
+		HttpSession session = request.getSession(false);
+		if (session == null) {
+			response.sendRedirect("/AutomatedStudentRegistrationSystem/LogIn");
 		}
-		
-		else
-		{
-			String user=(String)session.getAttribute("USERNAME");
-			String dsg=(String)session.getAttribute("DESG");
-			System.out.println(user+" "+dsg);
-			if(dsg.equals("Councilor"))
-		
+
+		else {
+			String user = (String) session.getAttribute("USERNAME");
+			String dsg = (String) session.getAttribute("DESG");
+			System.out.println(user + " " + dsg);
+			if (dsg.equals("Councilor"))
 			{
-				RequestDispatcher rd=request.getRequestDispatcher("/Councilor/Home.jsp");
+				RequestDispatcher rd = request
+						.getRequestDispatcher("/Councilor/Home.jsp");
 				request.setAttribute("USERNAME", user);
 				rd.forward(request, response);
 			}
 			else
 			{
-				RequestDispatcher rd = request.getRequestDispatcher("/Registrar/Home.jsp");
+				RequestDispatcher rd = request
+						.getRequestDispatcher("/Registrar/Home.jsp");
 				request.setAttribute("USERNAME", user);
 				rd.forward(request, response);
 			}
@@ -59,12 +64,13 @@ public class Welcome extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-	
+
 	}
 
 }
